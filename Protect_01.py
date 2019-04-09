c0ee1f5bca48fde47d3c35e63cd129e5c06fbfd0
HALOXIAO <584714929@qq.com>
2019-04-09 13:54:58 +0800

    some add

--- Protect_01.py
+++ Protect_01.py
@@ -1,20 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
 project = Flask(__name__)
+file_path = "E:\imagin\test"
 
-
-@project.route('/api/Project', methods=['POST'])
+@project.route('/api/Project', methods=['POST'], strict_slashes=False)
 def build_project():
-    pass
-
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    f = request.files
+    f.save(os.path.abspath.join(file_path, f.filename))
+    return jsonify({"errno": 'wrong', "errmsg": "上传成功"})
 
 @project.route('api/Project', methods=['DELETE'])
 def delete_project():
-    pass
+    if os.path.exists(file_path):
+        pass
+    else:
+        return jsonify({"Destination file does not exist"})
 
 
 @project.route('api/Project', methods=['GET'])
 def get_project():
-    pass
+
 
 
 @project.route('api/Project', methods=['PUT'])
