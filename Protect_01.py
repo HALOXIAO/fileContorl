a002a8eb10159e8cfe6be7ec5a879cdd0f48f20f
HALOXIAO <584714929@qq.com>
2019-04-10 16:26:07 +0800

    GET、POST and DELETE is DONE

--- Protect_01.py
+++ Protect_01.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, jsonify, send_from_directory, Response, abort
 import os
 import random
-import hashlib
 project = Flask(__name__)
-file_path = r"E:\imagin\test"
+file_path = "E:\imagin\\test"
 
 
 @project.route('/api/Project', methods=['POST'], strict_slashes=False)
 def build_project():
     if not os.path.exists(file_path):
         os.makedirs(file_path)
-    number = random.randint()
+    number = random.randint(100, 999999)
     f = request.files['file']
-    name = hash(str(number))
+    name = str(hash(number))
     f.save(os.path.join(file_path, name))
     path = file_path + '/' + name
     if os.path.exists(path):
@@ -22,22 +21,22 @@ def build_project():
     abort(Response("upload failed"))
 
 
-@project.route('/api/Project/<ID>', methods=['DELETE'])
-def delete_project(ID):
-    name = ID
-    if os.path.exists(file_path):
-        file = file_path + name
+@project.route('/api/Project', methods=['DELETE'], strict_slashes=False)
+def delete_project():
+    name = str(request.args.get('ID'))
+    file = file_path +'\\'+ name
+    if os.path.exists(file):
         os.remove(file)
+        return jsonify({'msg': '成功删除'})
     else:
-        return jsonify({"Destination file does not exist"})
-    return jsonify({'test'})
+        return jsonify({'fileError': "Destination file does not exist"})
+    return jsonify({'err': 'test'})
 
 
-@project.route('/api/Project/<ID>', methods=['GET'], strict_slashes=False)
-def get_project(ID):
-    name = ID
+@project.route('/api/Project', methods=['GET'], strict_slashes=False)
+def get_project():
+    name = str(request.args.get('ID'))
     if os.path.exists(file_path):
-        dirpath = os.path.join(project.root_path, 'upload')
         return send_from_directory(file_path, name, as_attachment=True)#as_attachment为True是下载
     else:
         return jsonify({"errormessage": "file does not exit"})
