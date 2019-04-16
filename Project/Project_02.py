35b55e6bf9aa7f323d165389099ec3f3f74dc995
HALOXIAO <584714929@qq.com>
2019-04-16 12:32:13 +0800

    DONE

--- Project/Project_02.py
+++ Project/Project_02.py
@@ -1,13 +1,9 @@
+
 from flask import Flask, request, jsonify, send_from_directory, Response, abort
 import os
 import random
-
 project = Flask(__name__)
 file_path = "E:\imagin\\test"
-
-
-class Change:
-    pass
 
 
 class Func:
@@ -34,9 +30,9 @@ class Func:
         abort(Response("faile"))
 
 
-
 @project.route('/api/Project', methods=['POST'], strict_slashes=False)
 def build_project():
+    func = Func()
     if not os.path.exists(file_path):
         os.makedirs(file_path)
     number = random.randint(100, 999999)
@@ -44,7 +40,7 @@ def build_project():
     name = str(hash(number))
     f.save(os.path.join(file_path, name))
     path = file_path + '/' + name
-    self.back(path, '上传成功', 'wrong', name)
+    func.back(path, '上传成功', 'wrong', name)
 
 @project.route('/api/Project', methods=['DELETE'], strict_slashes=False)
 def delete_project():
@@ -55,13 +51,14 @@ def delete_project():
         return jsonify({'msg': '成功删除'})
     else:
         return jsonify({'fileError': "Destination file does not exist"})
-    return jsonify({'err': 'wrong'})
+    return jsonify({'err': 'test'})
+
 
 @project.route('/api/Project', methods=['GET'], strict_slashes=False)
-def get_project(self):
+def get_project():
     name = str(request.args.get('ID'))
     if os.path.exists(file_path):
-        f = open(os.path.join(file_path, name), encoding='cp852')
+        f = open(os.path.join(file_path, name), encoding = 'cp852')
         w = open(os.path.join(file_path, 'TEMP'), 'a')
         for line in f:
             w.write(line)
@@ -73,19 +70,24 @@ def get_project(self):
             os.remove(os.path.join(file_path, name))
     else:
         return jsonify({"errormessage": "file does not exit"})
-        abort(Response("Download failed"))
+    abort(Response("Download failed"))
+
 
 @project.route('/api/Project', methods=['PUT'], strict_slashes=False)
 def edit_project():
+    func = Func()
     name = str(request.args.get('ID'))
     f = request.files['file']
-    f.save(file_path, 'file path wrong','成功编辑', f, name)
+    func.save(file_path, 'file path wrong','成功编辑', f, name)
+
 
 @project.route('/api/ForkProject', methods=['PUT'], strict_slashes=False)
 def fork_project():
-     name = str(request.args.get('ID'))
-     f.send(file_path, 'file does not exit', name)
+    name = str(request.args.get('ID'))
+    func = Func()
+    func.send(file_path, 'file does not exit', name)
 
 
 if __name__ == '__main__':
-    project.run(host='127.0.0.1', port = 5000)
+    project.run(host='127.0.0.1', port=5000)
+
