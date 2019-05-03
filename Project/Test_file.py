3132ecfcb4baaf2d6cbffd5b515380f1901928b8
HALOXIAO <584714929@qq.com>
2019-05-03 20:20:48 +0800

    test

--- Project/Test_file.py
+++ Project/Test_file.py
@@ -1,11 +1,33 @@
-import unittest
-import flask
-import projectGit.Project.Project_02
 import requests
 url = "http://127.0.0.1:5001/"
 middle1 = "api/Project"
+middle2 = "api/ForkProject"
+
+def choise_file():
+    file = open(r"E:\imagin\test/%s" %(input()))
+    data = {
+        'file': file
+    }
+    return data
+
 
 class TEST:
+    def get_project_test(self, id):
+        response = requests.get(url+ middle1 +"?ID="+str(id))
+        print(response.content)
+
+    def delete_project_test(self, id):
+        response = requests.delete(url + middle1 +"?ID="+str(id))
+
     def build_project_test(self):
-        response = requests.get(url+ middle1 +"?ID=136917")
-        print(requests)
+        response = requests.post(url=url+middle1, data=choise_file())
+
+    def edit_project_test(self, id):
+        response = requests.put(url=url+middle1+"?ID="+str(id))
+        print(response.content)
+    def fork_project_test(self, id):
+        response = requests.put(url=url + middle2 + "?ID=" + str(id))
+
+if __name__ == '__main__':
+    a = TEST()
+    a.get_project_test(12332112)
